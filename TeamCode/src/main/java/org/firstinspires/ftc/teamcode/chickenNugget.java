b269348aa722216d315920f7c98f4de66a4a31f2
Taylor <taylorjohns310@gmail.com>
2019-10-13 18:32:00 -0700

    heloo my is taylor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chickenNugget.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chickenNugget.java
@@ -29,7 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import 4com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
